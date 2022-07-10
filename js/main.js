96c7ceb8804a26a5467709149bf4645291213a80
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-07-10 16:18:01 +0300

    Add js styles

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
+
+elUrlShortenerForm.addEventListener('sumbit', function (evt) {
+  evt.preventDefault();
+
+  console.log('test');
+});
