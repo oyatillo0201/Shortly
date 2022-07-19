32ef1cbbc4bbbc592db6780aa25d2c348a8e48e7
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-07-19 12:14:10 +0300

    Add changes button clipboard

--- js/main.js
+++ js/main.js
@@ -21,6 +21,9 @@ if (elUrlShortener) {
       //Change background color
       evt.target.classList.add('url-shortener__copy-button--copied');
 
+      // Copy short link to clipboard
+      navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+
       // Reset button text and class after 1 second
       setTimeout(function () {
         evt.target.textContent = 'Copy';
